724a1a2889cc55d46a12efcfd4311dff0775cd73
Yongfeng LI <wliyongfeng@gamil.com>
2012-11-11 13:40:13 +0000

    try to navagate

--- examples/src/main/java/li/test/examples/jsoup/HelloJsoup.java
+++ examples/src/main/java/li/test/examples/jsoup/HelloJsoup.java
@@ -1,12 +1,16 @@
 package li.test.examples.jsoup;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 
 public class HelloJsoup {
     private Document doc;
+    private static int count = 1;
 
     public static void main(String[] args) {
         HelloJsoup jsoup = new HelloJsoup();
@@ -23,6 +27,27 @@ public class HelloJsoup {
     }
 
     public void getElements(String elementName) {
-        System.out.println(doc.select(elementName));
+        Element body = doc.body();
+        System.out.println(body.nodeName());
+//        printChildNode(body);
+
+    }
+
+    public void printChildNode(Node node) {
+        Iterator<Node> nodes = node.childNodes().iterator();
+        Node child;
+        count++;
+        while (nodes.hasNext()) {
+            child = nodes.next();
+            System.out.println(child.nodeName());
+//            printSpace(count);
+//            printChildNode(child);
+        }
+    }
+
+    public void printSpace(int cnt) {
+        for (int i = 0; i < cnt; i++) {
+            System.out.print(" ");
+        }
     }
 }
