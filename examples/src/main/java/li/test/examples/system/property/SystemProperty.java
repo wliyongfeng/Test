3763a1411764477272be9c3f85b71c01d6d1ced2
Yongfeng LI <wliyongfeng@gamil.com>
2012-12-02 11:16:00 +0000

    chage from clearProperty to getProperty

--- examples/src/main/java/li/test/examples/system/property/SystemProperty.java
+++ examples/src/main/java/li/test/examples/system/property/SystemProperty.java
@@ -2,20 +2,20 @@ package li.test.examples.system.property;
 
 public class SystemProperty {
     public static void main(String[] args) {
-        System.out.println(System.clearProperty("file.separator"));
-        System.out.println(System.clearProperty("java.class.path"));
-        System.out.println(System.clearProperty("java.vendor"));
-        System.out.println(System.clearProperty("java.home"));
-        System.out.println(System.clearProperty("java.vendor.url"));
-        System.out.println(System.clearProperty("java.version"));
-        System.out.println(System.clearProperty("line.separator"));
-        System.out.println(System.clearProperty("os.arch"));
-        System.out.println(System.clearProperty("os.name"));
-        System.out.println(System.clearProperty("os.version"));
-        System.out.println(System.clearProperty("path.separator"));
-        System.out.println(System.clearProperty("user.dir"));
-        System.out.println(System.clearProperty("user.home"));
-        System.out.println(System.clearProperty("user.name"));
+        System.out.println(System.getProperty("file.separator"));
+        System.out.println(System.getProperty("java.class.path"));
+        System.out.println(System.getProperty("java.vendor"));
+        System.out.println(System.getProperty("java.home"));
+        System.out.println(System.getProperty("java.vendor.url"));
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("line.separator"));
+        System.out.println(System.getProperty("os.arch"));
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("os.version"));
+        System.out.println(System.getProperty("path.separator"));
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("user.name"));
         
         System.setProperty("li.value", "great");
         System.out.println(System.getProperty("li.value"));
